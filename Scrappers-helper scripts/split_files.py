ce974ed5a98a49554a939c167c0f269f4594497a
Andrey <99732328+andrey-09@users.noreply.github.com>
2025-06-26 02:28:23 +0200

    updates to databases

--- Scrappers-helper scripts/split_files.py
+++ Scrappers-helper scripts/split_files.py
@@ -1,14 +1,15 @@
 files=15
-file_name='new.txt'
+file_name='..\\BOOKS TO DOWNLOAD\\batch_2.txt'
 
 with open(file_name,"r") as file:
     urls=file.read().splitlines()
 
 coef=len(urls)//files
 
+folder="..\\BOOKS TO DOWNLOAD\\"
 name=file_name.split(".txt")[0]
 for i in range(files):
-    with open(name+"_file_"+str(i)+".txt","w") as file1:
+    with open(folder+name+"_file_"+str(i)+".txt","w") as file1:
         if i!=files-1:
             file1.write("\n".join(urls[coef*(i):coef*(i+1)]))
         else:
